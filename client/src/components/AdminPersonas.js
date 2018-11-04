6776b71a9865a33994dda168acc7c544e9575445
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2018-11-04 14:57:09 -0600

    Cambios del Home

--- client/src/components/AdminPersonas.js
+++ client/src/components/AdminPersonas.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { Button, Form, FormGroup, Label, Input, FormText, CustomInput } from 'reactstrap';
 
 const styles = {
   card: {
@@ -21,9 +22,61 @@ class AdminPersonas extends Component {
 
   render() {
     return (
-      <div>
-        <h1>AdminPersonas</h1>
-      </div>
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Nombre Completo</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">Identidad</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">Telefono</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">Direccion</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleCheckbox">Registros Sanitarios</Label>
+          <div>
+            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleCheckbox">Registros Legales</Label>
+          <div>
+            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleCheckbox">Registros Academicos</Label>
+          <div>
+            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleCheckbox">Registros Profesionales y Laborales</Label>
+          <div>
+            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
+            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
+          </div>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
     );
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import { Button, Form, FormGroup, Label, Input, FormText, CustomInput } from 'reactstrap';
 
 const styles = {
   card: {
@@ -21,66 +20,13 @@ class Home extends Component {
 
   render() {
     return (
-      <Form>
-        <FormGroup>
-          <Label for="exampleEmail">Nombre Completo</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Identidad</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Telefono</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Direccion</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleCheckbox">Registros Sanitarios</Label>
-          <div>
-            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
-          </div>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleCheckbox">Registros Legales</Label>
-          <div>
-            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
-          </div>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleCheckbox">Registros Academicos</Label>
-          <div>
-            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
-          </div>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleCheckbox">Registros Profesionales y Laborales</Label>
-          <div>
-            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
-            <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one" disabled />
-          </div>
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
+      <div>
+        hola
+      </div>
     );
   }
+
 }
-
-
 
 Home.propTypes = {
   classes: PropTypes.object.isRequired
