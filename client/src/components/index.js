e98f7d02987c84aeb1cd55ad99eb998ef27a65c8
canozo <canwoahzo@gmail.com>
2018-11-04 14:19:15 -0600

    Agregar todas las nuevas paginas

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,6 +6,14 @@ import { Route, Link, Switch } from 'react-router-dom';
 import Pagina from './Pagina';
 import Test from './Test';
 import Home from './Home';
+import Seleccion from './Seleccion';
+import SolicitarPuesto from './SolicitarPuesto';
+import SolicitarEmpleo from './SolicitarEmpleo';
+import AdminPuestos from './AdminPuestos';
+import AdminEmpleos from './AdminEmpleos';
+import AdminPersonas from './AdminPersonas';
+import AdminEmpresas from './AdminEmpresas';
+import AdminRequisitos from './AdminRequisitos';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import { Toolbar, Typography } from '@material-ui/core';
@@ -32,6 +40,8 @@ class App extends Component {
     this.classes = this.props.classes;
   }
 
+  // import AdminRequisitos from './AdminRequisitos';
+
   render() {
     const topbarButtons = (
       <div>
@@ -44,6 +54,30 @@ class App extends Component {
         <Link to="/test">
           <Button style={{ color: '#fff' }} >Test</Button>
         </Link>
+        <Link to="/seleccion">
+          <Button style={{ color: '#fff' }} >Seleccion</Button>
+        </Link>
+        <Link to="/solicitarpuesto">
+          <Button style={{ color: '#fff' }} >Solicitar Puesto</Button>
+        </Link>
+        <Link to="/solicitarempleo">
+          <Button style={{ color: '#fff' }} >Solicitar Empleo</Button>
+        </Link>
+        <Link to="/adminpuestos">
+          <Button style={{ color: '#fff' }} >Administrar Puestos</Button>
+        </Link>
+        <Link to="/adminempleos">
+          <Button style={{ color: '#fff' }} >Administrar Empleos</Button>
+        </Link>
+        <Link to="/adminpersonas">
+          <Button style={{ color: '#fff' }} >Administrar Personas</Button>
+        </Link>
+        <Link to="/adminempresas">
+          <Button style={{ color: '#fff' }} >Administrar Empresas</Button>
+        </Link>
+        <Link to="/adminrequisitos">
+          <Button style={{ color: '#fff' }} >Administrar Requisitos</Button>
+        </Link>
       </div>
     );
 
@@ -55,7 +89,7 @@ class App extends Component {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" className={this.classes.flex}>
-              Proyecto 1
+              Proyecto
             </Typography>
             {topbarButtons}
           </Toolbar>
@@ -69,6 +103,14 @@ class App extends Component {
               <Route path="/" exact component={Home} />
               <Route path="/pagina" exact component={Pagina} />
               <Route path="/test" exact component={Test} />
+              <Route path="/seleccion" exact component={Seleccion} />
+              <Route path="/solicitarpuesto" exact component={SolicitarPuesto} />
+              <Route path="/solicitarempleo" exact component={SolicitarEmpleo} />
+              <Route path="/adminpuestos" exact component={AdminPuestos} />
+              <Route path="/adminempleos" exact component={AdminEmpleos} />
+              <Route path="/adminpersonas" exact component={AdminPersonas} />
+              <Route path="/adminempresar" exact component={AdminEmpresas} />
+              <Route path="/adminrequisitos" exact component={AdminRequisitos} />
               <Route render={() => <h3>Pagina no existe!</h3>} />
             </Switch>
           </div>
