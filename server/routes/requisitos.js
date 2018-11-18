652c5f50ce88d633f8f367cfa599ea873f6b3315
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2018-11-18 17:02:57 -0600

    esta feo redis

--- server/routes/requisitos.js
+++ server/routes/requisitos.js
@@ -2,7 +2,58 @@ var express = require('express');
 var util = require('util');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/sanitarios', function(req, res) {
+  let requisito1 = req.body.requisito1;
+  let requisito2 = req.body.requisito2;
+  let requisito3 = req.body.requisito3;
+
+  // obtener la data del server
+  // procesarla
+  // enviarla
+
+  res.json({
+    status: 200
+  });
+});
+
+router.get('/legales', function(req, res) {
+  // obtener la data del server
+  // procesarla
+  // enviarla
+
+  res.json({
+    status: 200
+  });
+});
+
+router.get('/academicos', function(req, res) {
+  // obtener la data del server
+  // procesarla
+  // enviarla
+
+  res.json({
+    status: 200
+  });
+});
+
+router.get('/profesionales', function(req, res) {
+  // obtener la data del server
+  // procesarla
+  // enviarla
+
+  res.json({
+    status: 200
+  });
+});
+
+router.get('/laborales', function(req, res) {
+  // obtener la data del server
+  // procesarla
+  // enviarla
+
+  res.json({
+    status: 200
+  });
 });
 
 module.exports = router;
