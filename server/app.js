33c9c363516f0c9b4891f71a666a98d9ca80b122
noheliaeuceda <noheliaeucedaf@gmail.com>
2018-11-20 00:31:43 -0600

    personas app

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var puestosRouter = require('./routes/puestos');
 var requisitosRouter = require('./routes/requisitos');
 var empresasRouter = require('./routes/empresas');
+var personasRouter = require('./routes/personas');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/requisitos', requisitosRouter);
 app.use('/puestos', puestosRouter);
 app.use('/empresas', empresasRouter);
+app.use('/personas', personasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
