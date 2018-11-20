f15955efe5c5c3c71453fea167ca2f97d5dc0408
canozo <canwoahzo@gmail.com>
2018-11-20 17:57:36 -0600

    Get de empresas

--- server/routes/empresas.js
+++ server/routes/empresas.js
@@ -2,6 +2,15 @@ var express = require('express');
 var util = require('util');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  // obtener las empresas guardados en el servidor y enviarlos
+  client.hgetall('empresas', function(err, obj) {
+    if (!err) {
+      res.json(obj);
+    }
+  });
+});
+
 router.put('/', function(req, res) {
   // guardar la informacion obtenida en redis
   console.log("servidor");
